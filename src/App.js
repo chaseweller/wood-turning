cb78d0734aeff99e54c5252a77155cb33943e465
Unknown <r.chase.weller@gmail.com>
2018-08-21 22:29:41 -0600

    Further work with routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
 
 class App extends Component {
+
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,7 +17,11 @@ class App extends Component {
           <h1 className="App-title">Wood Turners</h1>
         </header>
         <p className="App-intro">
-          This is my website for projects. Projects I want to do and projects completed.  
+          This is my website for projects. Projects I want to do and projects completed. 
+          <ul>
+          <li>  ABOUT </li>
+          <li> WHAT SHOULD I DO? </li> 
+          </ul>
         </p>
       </div>
     );
--- src/routes/homepage/index.js
+++ src/routes/homepage/index.js
@@ -3,8 +3,18 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const home = () => (
     <Router>
+        <div>
+            <ul>
+                <li>
+                    <Link to ="/"> HOME </Link>
+                </li>
+                 <li>
+                    <Link to = "/about"> ABOUT </Link>
+                </li>
+             </ul>
+        </div>
+    </Router>
 
-        </Router>
-        
 
-)
+);
+
